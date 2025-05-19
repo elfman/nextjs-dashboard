55a085da0f9c07b3a56793480c733a0f2dafdc1b
Harlan <luoxwen@foxmail.com>
2025-05-19 23:34:21 +0800

    feat(dashboard): 添加仪表板页面和加载组件
    - 新增仪表板加载组件和主页面
    - 实现卡片数据异步加载和展示
    - 添加营收图表和最新发票组件
    - 使用 Skeleton 组件实现加载状态展示

---
+++ app/dashboard/cards.tsx
@@ -0,0 +1,21 @@
+import { fetchCardData } from "../lib/data";
+import { Card } from "../ui/dashboard/cards";
+
+export default async function CardWrapper() {
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
+  return (
+    <>
+    <Card title="Collected" value={totalPaidInvoices} type="collected" />
+    <Card title="Pending" value={totalPendingInvoices} type="pending" />
+    <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+    <Card title="Total Customers" value={numberOfCustomers} type="customers" />
+    </>
+  )
+}
